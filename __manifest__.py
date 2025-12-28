cec4934d8c25fa77ad869cd4eab26b531b8401e5
tonitomystery <amaristany01@gmail.com>
2025-12-28 08:51:17 -0400

    Update module manifest for client reminder feature

--- __manifest__.py
+++ __manifest__.py
@@ -7,7 +7,6 @@
         "data/ir_cron.xml",
         "views/invoice_reminder_config_views.xml",
         "views/partnerter_reminder_views.xml",
-        "#views/partner_view.xml"
     ],
     "installable": True,
 }
